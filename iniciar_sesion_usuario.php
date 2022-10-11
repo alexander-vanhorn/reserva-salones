58eeacc1320888619a03622688310519b04d6d2b
albert3985 <68358380+albert3985@users.noreply.github.com>
2022-10-11 00:01:58 -0500

    hi
    inicio de sesion de usuario con css basico.

---
+++ iniciar_sesion_usuario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="..//CSS/inicio_sesion.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesion</title>
+</head>
+<body>
+    <form action="post" name="sesion" >
+        <h1>Iniciar Sesion</h1>
+        <label for="username">Usuario <input type="text"></label> <br>
+        <label for="password">Contraseña<input type="password"></label> <br>
+        <input type="submit" value="Enviar" >
+
+    </form>
+    
+    
+</body>
+</html>
