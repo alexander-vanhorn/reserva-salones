11fa6bea3a88017f87b177e183315a03c756faac
Alexander Vanhorn <alexander.vanhorn@utp.ac.pa>
2022-10-13 11:40:00 -0500

    ajustes de la pantalla de historial de reservas

--- index.php
+++ index.php
@@ -8,25 +8,84 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
+    <title>Historial de reservaciones</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="https://colorlib.com/etc/tb/Table_Responsive_v1/images/icons/favicon.ico">
+
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/select2.min.css">
+
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/perfect-scrollbar.css">
+
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/util.css">
+    <link rel="stylesheet" type="text/css" href="Table%20V01_files/main.css">
+
+    <meta name="robots" content="noindex, follow">
+    <script type="text/javascript" async="" src="Table%20V01_files/analytics.js" nonce="0ece9386-e76c-4eb0-9826-a3dfe74ffc50"></script><script defer="defer" referrerpolicy="origin" src="Table%20V01_files/s.js"></script><script nonce="0ece9386-e76c-4eb0-9826-a3dfe74ffc50">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{};a.zarazData.executed=[];a.zaraz={deferred:[],listeners:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","debug"])a.zaraz[e]=a.zaraz._f(e);a.zaraz.init=()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text);a.zarazData.x=Math.random();a.zarazData.w=a.screen.width;a.zarazData.h=a.screen.height;a.zarazData.j=a.innerHeight;a.zarazData.e=a.innerWidth;a.zarazData.l=a.location.href;a.zarazData.r=e.referrer;a.zarazData.k=a.screen.colorDepth;a.zarazData.n=e.characterSet;a.zarazData.o=(new Date).getTimezoneOffset();a.zarazData.q=[];for(;a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e||{}).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin";z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData)));t.parentNode.insertBefore(z,t)};["complete","interactive"].includes(e.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
+
 </head>
-<body>
-    Hola mundo ideal
-    <div class="container text-center">
-  <div class="row">
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-  </div>
+
+<tbody>
+<tr>
+<td class="column1">2017-09-29 01:22</td>
+<td class="column2">200398</td>
+<td class="column3">3-319</td>
+<td class="column4">$999.00</td>
+<td class="column5">1</td>
+<td class="column6">$999.00</td>
+</tr>
+<tr>
+<td class="column1">2017-09-28 05:57</td>
+<td class="column2">200397</td>
+<td class="column3">3-320</td>
+<td class="column4">$756.00</td>
+<td class="column5">1</td>
+<td class="column6">$756.00</td>
+</tr>
+<tr>
+<td class="column1">2017-09-26 05:57</td>
+<td class="column2">200396</td>
+<td class="column3">3-315</td>
+<td class="column4">$22.00</td>
+<td class="column5">2</td>
+<td class="column6">$44.00</td>
+</tr>
+<tr>
+
+</tr>
+</tbody>
+</table>
 </div>
+</div>
+</div>
+</div>
+
+<script src="Table%20V01_files/jquery-3.2.1.min.js"></script>
+
+<script src="Table%20V01_files/popper.js"></script>
+<script src="Table%20V01_files/bootstrap.min.js"></script>
+
+<script src="Table%20V01_files/select2.min.js"></script>
+
+<script src="Table%20V01_files/main.js"></script>
+
+<script async="" src="Table%20V01_files/js"></script>
+<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+<script defer="defer" src="Table%20V01_files/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon="{&quot;rayId&quot;:&quot;759964e13b52db2d&quot;,&quot;token&quot;:&quot;cd0b4b3a733644fc843ef0b185f98241&quot;,&quot;version&quot;:&quot;2022.8.1&quot;,&quot;si&quot;:100}" crossorigin="anonymous"></script>
+
+
 </body>
+
 </html>
