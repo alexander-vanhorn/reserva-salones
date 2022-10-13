daf5d5d49e1ab62f2660018604753febc26ec243
Alexander Vanhorn <alexander.vanhorn@utp.ac.pa>
2022-10-13 11:44:23 -0500

    adición del body

--- index.php
+++ index.php
@@ -30,31 +30,46 @@
     <script type="text/javascript" async="" src="Table%20V01_files/analytics.js" nonce="0ece9386-e76c-4eb0-9826-a3dfe74ffc50"></script><script defer="defer" referrerpolicy="origin" src="Table%20V01_files/s.js"></script><script nonce="0ece9386-e76c-4eb0-9826-a3dfe74ffc50">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{};a.zarazData.executed=[];a.zaraz={deferred:[],listeners:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","debug"])a.zaraz[e]=a.zaraz._f(e);a.zaraz.init=()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text);a.zarazData.x=Math.random();a.zarazData.w=a.screen.width;a.zarazData.h=a.screen.height;a.zarazData.j=a.innerHeight;a.zarazData.e=a.innerWidth;a.zarazData.l=a.location.href;a.zarazData.r=e.referrer;a.zarazData.k=a.screen.colorDepth;a.zarazData.n=e.characterSet;a.zarazData.o=(new Date).getTimezoneOffset();a.zarazData.q=[];for(;a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e||{}).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin";z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData)));t.parentNode.insertBefore(z,t)};["complete","interactive"].includes(e.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
 
 </head>
+<body>
+<div class="limiter">
+<div class="container-table100">
+<div class="wrap-table100">
+<div class="table100">
+<table>
+<thead>
+<tr class="table100-head">
+<th class="column1">Fecha</th>
+<th class="column2">ID Reserva</th>
+<th class="column3">Salón</th>
+<th class="column4">Motivo</th>
+<th class="column5">Cantidad personas</th>
 
+</tr>
+</thead>
 <tbody>
 <tr>
 <td class="column1">2017-09-29 01:22</td>
 <td class="column2">200398</td>
 <td class="column3">3-319</td>
-<td class="column4">$999.00</td>
+<td class="column4">Parcial fuera de hora de clase</td>
 <td class="column5">1</td>
-<td class="column6">$999.00</td>
+
 </tr>
 <tr>
 <td class="column1">2017-09-28 05:57</td>
 <td class="column2">200397</td>
 <td class="column3">3-320</td>
-<td class="column4">$756.00</td>
+<td class="column4">Clase</td>
 <td class="column5">1</td>
-<td class="column6">$756.00</td>
+
 </tr>
 <tr>
 <td class="column1">2017-09-26 05:57</td>
 <td class="column2">200396</td>
 <td class="column3">3-315</td>
-<td class="column4">$22.00</td>
+<td class="column4">Seminario</td>
 <td class="column5">2</td>
-<td class="column6">$44.00</td>
+
 </tr>
 <tr>
 
