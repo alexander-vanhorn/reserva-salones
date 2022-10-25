5093f81d0d822dec1ec4fff9aec24dec60e57c57
Alexander Vanhorn <alexander.vanhorn@utp.ac.pa>
2022-10-25 11:35:55 -0500

    pantallas de historial de reservas del lado del admin y horario disponibilidad

---
+++ historial_reservas_admin.php
@@ -0,0 +1,85 @@
+<?php
+ $x = 5;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>Universidad Tecnológica de Panamá</h2>
+    <h3>Sistema automatizado de reserva de salones</h3><br>
+
+    <br><br>
+
+    <h4>Historial de reservaciones</h4>
+
+    <div class="limiter">
+<div class="container-table100">
+<div class="wrap-table100">
+<div class="table100">
+<table>
+<thead>
+<tr class="table100-head">
+<th class="column1">Fecha</th>
+<th class="column2">ID Reserva</th>
+<th class="column3">Salón</th>
+<th class="column4">Motivo</th>
+<th class="column5">Cantidad personas</th>
+<th class="column6">Nombre</th>
+<th class="column7">Cédula</th>
+
+
+</tr>
+</thead>
+<tbody>
+<tr>
+<td class="column1">2017-09-29 01:22</td>
+<td class="column2">200398</td>
+<td class="column3">3-319</td>
+<td class="column4">Parcial fuera de hora de clase</td>
+<td class="column5">1</td>
+<td class="column6">Juan Peréz</th>
+<td class="column7">9-999-999</th>
+ 
+</tr>
+<tr>
+<td class="column1">2017-09-28 05:57</td>
+<td class="column2">200397</td>
+<td class="column3">3-320</td>
+<td class="column4">Clase</td>
+<td class="column5">1</td>
+<td class="column6">Pepe Martínez</th>
+<td class="column7">7-777-777</th>
+
+</tr>
+<tr>
+<td class="column1">2017-09-26 05:57</td>
+<td class="column2">200396</td>
+<td class="column3">3-315</td>
+<td class="column4">Seminario</td>
+<td class="column5">2</td>
+<td class="column6">Nemesio Cortez</th>
+<td class="column7">4-444-444</th>
+
+</tr>
+<tr>
+
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+
+<br>
+
+<button>Descargar historial</button>
+
+</body>
+</html>
