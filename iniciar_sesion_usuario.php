cd77a971fb9849d620ccab9f9d00d371f1aa6641
Alexander Vanhorn <alexander.vanhorn@utp.ac.pa>
2022-10-27 10:43:55 -0500

    cambios en iniciar_sesion_usuario

--- iniciar_sesion_usuario.php
+++ iniciar_sesion_usuario.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="..//CSS/inicio_sesion.css">
+    <link rel="stylesheet" href="css\inicio_sesion.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+
     <title>Iniciar sesion</title>
 </head>
 <body>
     <form action="post" name="sesion" >
-        <h1>Iniciar Sesion</h1>
+        <h3>Iniciar Sesion</h3>
         <label for="username">Usuario <input type="text"></label> <br>
         <label for="password">Contraseña<input type="password"></label> <br>
         <input type="submit" value="Enviar" >
