1ac9719d75d9bcbbc5b0a34024cc45096eefb4fc
Alexander Vanhorn <alexander.vanhorn@utp.ac.pa>
2022-10-27 10:39:37 -0500

    eliminacion de algunos elementos y adicion de otros nuevos

--- historial_reservas_admin.php
+++ historial_reservas_admin.php
@@ -8,78 +8,82 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+    <title>Historial de reserva de salones</title>
+
+    <link rel="shortcut icon" href="images\logo_utp_1_300.png" type="image/x-icon">
+    <link rel="stylesheet" href="css\historial-reservas-admin.css">
+
+    
 </head>
 <body>
-<h2>Universidad Tecnológica de Panamá</h2>
-    <h3>Sistema automatizado de reserva de salones</h3><br>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>    
+<br>
+<div class="encabezado">
+    <img src="images\logo_utp_1_300.png" class="logo-utp">
+    
+    <h2>&nbsp; Universidad Tecnológica de Panamá</h2>
+    <h3>&nbsp; Sistema automatizado de reserva de salones</h3><br>
+</div>
+
 
     <br><br>
 
     <h4>Historial de reservaciones</h4>
 
-    <div class="limiter">
-<div class="container-table100">
-<div class="wrap-table100">
-<div class="table100">
-<table>
-<thead>
-<tr class="table100-head">
-<th class="column1">Fecha</th>
-<th class="column2">ID Reserva</th>
-<th class="column3">Salón</th>
-<th class="column4">Motivo</th>
-<th class="column5">Cantidad personas</th>
-<th class="column6">Nombre</th>
-<th class="column7">Cédula</th>
-
-
-</tr>
-</thead>
-<tbody>
-<tr>
-<td class="column1">2017-09-29 01:22</td>
-<td class="column2">200398</td>
-<td class="column3">3-319</td>
-<td class="column4">Parcial fuera de hora de clase</td>
-<td class="column5">1</td>
-<td class="column6">Juan Peréz</th>
-<td class="column7">9-999-999</th>
- 
-</tr>
-<tr>
-<td class="column1">2017-09-28 05:57</td>
-<td class="column2">200397</td>
-<td class="column3">3-320</td>
-<td class="column4">Clase</td>
-<td class="column5">1</td>
-<td class="column6">Pepe Martínez</th>
-<td class="column7">7-777-777</th>
-
-</tr>
-<tr>
-<td class="column1">2017-09-26 05:57</td>
-<td class="column2">200396</td>
-<td class="column3">3-315</td>
-<td class="column4">Seminario</td>
-<td class="column5">2</td>
-<td class="column6">Nemesio Cortez</th>
-<td class="column7">4-444-444</th>
-
-</tr>
-<tr>
-
-</tr>
-</tbody>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col1">Fecha</th>
+      <th scope="col2">ID</th>
+      <th scope="col3">Nombre</th>
+      <th scope="col4">Cédula</th>
+      <th scope="col4">Salón</th>
+      <th scope="col4">Motivo</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>20-07-2022</td>
+      <td>20887496</td>
+      <td>Juan Peréz</td>
+      <td>9-999-999</td>
+      <td>3-319</td>
+      <td>Estudio</td>
+    </tr>
+    <tr>
+      <td>21-07-2022</td>
+      <td>20887497</td>
+      <td>Nemesio Cruz</td>
+      <td>8-888-888</td>
+      <td>3-318</td>
+      <td>Estudio</td>
+    </tr>
+    <tr>
+      <td>22-07-2022</td>
+      <td>20887498</td>
+      <td>Nancy Davolio</td>
+      <td>7-777-777</td>
+      <td>3-303</td>
+      <td>Estudio</td>
+    </tr>
+  </tbody>
 </table>
-</div>
-</div>
-</div>
-</div>
 
 <br>
 
-<button>Descargar historial</button>
+<button type="button" class="btn btn-primary">Atrás</button>
+<button type="button" class="btn btn-primary">Descargar historial</button>
+
+<br><br><br><br><br><br><br>
+
+<footer> @Code4 | Desarrollo de Software VII | 1LS132 | 2022</footer>
+<br>
+
+
 
 </body>
 </html>
